b656ff4d15934a5733f48436c06e0ebdc8c66b93
MaiLignini <mairalignini@gmail.com>
2023-07-27 22:28:36 +0000

    E7

--- src/workflow-semillerio/741_TS_training_strategy.r
+++ src/workflow-semillerio/741_TS_training_strategy.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "TS7410_6"
+PARAM$experimento <- "TS7410_7"
 
-PARAM$exp_input <- "FE7310_2"
+PARAM$exp_input <- "FE7310_3"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
